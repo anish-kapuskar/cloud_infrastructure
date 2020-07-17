07c13d275764e4ef54274827e765df103cb179ca
anishkapuskar <kapuskar.a@husky.neu.edu>
2020-07-17 14:33:43 -0400

    a8 commit

--- main.tf
+++ main.tf
@@ -4,12 +4,20 @@ provider "aws" {
 }
 
 module "vpc_module" {
-  source ="/home/anishkapuskar/cloud/tf/tf4/modules"
+  source ="/home/anishkapuskar/cloudapps/infrastructure/modules"
  vpc_cidr="${var.vpc_cidr}"
 subnet1_cidr="${var.subnet1_cidr}"
 s1az="${var.s1az}"
 subnet2_cidr="${var.subnet2_cidr}"
 s2az="${var.s2az}"
+subnet3_cidr="${var.subnet3_cidr}"
+s3az="${var.s3az}"
+subnet4_cidr="${var.subnet4_cidr}"
+s4az="${var.s4az}"
+subnet5_cidr="${var.subnet5_cidr}"
+s5az="${var.s5az}"
+subnet6_cidr="${var.subnet6_cidr}"
+s6az="${var.s6az}"
 custom_ami="${var.custom_ami}"
  
 } 
--- variables.tf
+++ variables.tf
@@ -19,6 +19,39 @@ variable  "s2az" {
     
 }
 
+variable  "subnet3_cidr" {
+    type = string
+    
+}
+variable  "s3az" {
+    type = string
+    
+}
+variable  "subnet4_cidr" {
+    type = string
+    
+}
+variable  "s4az" {
+    type = string
+    
+}
+variable  "subnet5_cidr" {
+    type = string
+    
+}
+variable  "s5az" {
+    type = string
+    
+}
+variable  "subnet6_cidr" {
+    type = string
+    
+}
+variable  "s6az" {
+    type = string
+    
+}
+
 variable "region" {
   type = string
 }
